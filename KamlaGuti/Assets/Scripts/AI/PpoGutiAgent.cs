bb16d5ee8c0999b4d7e96cbc8160bc8f497e907f
Samin Bin karim <saminbinkarim@gmail.com>
2021-02-11 18:22:33 +0600

    Created Address Translator class. Created Action Mask for PPO agent

--- KamlaGuti/Assets/Scripts/AI/PpoGutiAgent.cs
+++ KamlaGuti/Assets/Scripts/AI/PpoGutiAgent.cs
@@ -1,4 +1,6 @@
-﻿using Board.Guti;
+﻿using System.Collections.Generic;
+using Board.Guti;
+using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Random = UnityEngine.Random;
 
@@ -6,27 +8,30 @@ public class PpoGutiAgent : GutiAgent
 {
     public override void Initialize() => MaxStep = 0;
 
-    // public override void OnEpisodeBegin() => Debug.Log("haia");
-
-    public override void MakeMove() => RequestDecision();
+    public override void MakeMove()
+    {
+        gameManager.simulator.LoadMap();
+        RequestDecision();
+    }
 
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(gameManager.simulator.GetCurrentBoardStateAsList());
-        sensor.AddObservation((float)gutiType);
+        sensor.AddObservation((float) gutiType);
     }
 
     public override void OnActionReceived(float[] vectorAction)
     {
-        var source = (int)vectorAction[0];
-        var target =  (int)vectorAction[1];
+        var source = (int) vectorAction[0];
+        var target = (int) vectorAction[1];
         var move = gameManager.simulator.GetMoveFromIndexes(source, target);
         if (RuleBook.IsMoveValid(move, gutiType))
         {
             AgentMove(move);
-            var reward =  gameManager.scoreboard.GetScoreDifference(gutiType) / 16.0f;
+            var reward = gameManager.scoreboard.GetScoreDifference(gutiType) / 16.0f;
             SetReward(reward);
             gameManager.EndStep(gutiType, move);
+            gameManager.simulator.UnloadMap();
             gameManager.UnlockStep();
         }
         else
@@ -36,7 +41,29 @@ public class PpoGutiAgent : GutiAgent
         }
     }
 
-    protected override  Move AgentMove(Move move)
+    public override void CollectDiscreteActionMasks(DiscreteActionMasker actionMasker)
+    {
+        var mask = CreateMask();
+        actionMasker.SetMask(0, mask[0]);
+        actionMasker.SetMask(1, mask[1]);
+    }
+    
+    private List<List<int>> CreateMask()
+    {
+        var moveIndexes = gameManager.simulator.GetMoveIndexes(gutiType);
+        var sourceMask = new List<int>();
+        var targetMask = new List<int>();
+    
+        for (var i = 0; i < 37; i++)
+        {
+            if(!moveIndexes[0].Contains(i))sourceMask.Add(i);
+            if(!moveIndexes[1].Contains(i))targetMask.Add(i);
+        }
+        var mask = new List<List<int>>() {sourceMask, targetMask};
+        return mask;
+    }
+
+    protected override Move AgentMove(Move move)
     {
         gameManager.GetBoard().MoveGuti(move);
         gameManager.GetPlayer(gutiType).UpdateScore(move);
---
+++ KamlaGuti/Assets/Scripts/Board/AddressIndexTranslator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Board.Guti;
+
+namespace Board
+{
+    public class AddressIndexTranslator
+    {
+        private readonly Dictionary<Address, int> _addressToIndex;
+        private readonly Dictionary<int, Address> _indexToAddress;
+
+        public AddressIndexTranslator(IReadOnlyList<GutiNode> gutiArray)
+        {
+            _addressToIndex = new Dictionary<Address, int>(37);
+            _indexToAddress = new Dictionary<int, Address>(37);
+            Init(gutiArray);
+        }
+
+        private void Init(IReadOnlyList<GutiNode> gutiArray)
+        {
+            for (var index = 0; index < gutiArray.Count; index++)
+            {
+                var gutiNode = gutiArray[index];
+                _addressToIndex.Add(gutiNode.Address, index);
+                _indexToAddress.Add(index, gutiNode.Address);
+            }
+        }
+
+        public Address GetAddressFromIndex(int addressIndex) => _indexToAddress[addressIndex];
+		
+        public int GetIndexFromAddress(Address address) => _addressToIndex[address];
+
+    }
+}
